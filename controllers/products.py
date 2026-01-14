1835b6499e22fd2408e199abe5a8a16c4e1e54a8
Tanisha <tanishasura0@gmail.com>
2026-01-14 17:14:41 +0530

    Saved changes

--- controllers/products.py
+++ controllers/products.py
@@ -1,21 +1,22 @@
 from sqlalchemy.orm import Session
 from models.products import Product
+from fastapi import HTTPException
 
 def create_product(db: Session, product_data: dict):
     print(f"Received data: {product_data}")
 
     try:
         new_product = Product(**product_data)
-        print("Model created successfully.")
+        # print("Model created successfully.")
         
         db.add(new_product)
-        print(f"Attempting to COMMIT to Render Postgres (ID: {new_product.id})...")
+        # print(f"Attempting to COMMIT to Render Postgres (ID: {new_product.id})...")
         
         db.commit()
-        print(" Database Commit SUCCESSFUL!")
+        # print("Database Commit SUCCESSFUL!")
         
         db.refresh(new_product)
-        print(f"Final Product saved in DB: {new_product.name}")
+        # print(f"Final Product saved in DB: {new_product.name}")
         return new_product
 
     except Exception as e:
@@ -26,3 +27,36 @@ def create_product(db: Session, product_data: dict):
 
     finally:
         print(" DB DEBUG END ")
+
+
+
+def get_all_products(db: Session):
+    return db.query(Product).all()
+
+
+
+def update_product(db: Session, product_id: str, price: float, stock: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    
+    if not db_product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    db_product.price = price
+    db_product.stock_qty = stock
+
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+
+def delete_product(db: Session, product_id: str):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    
+    if not db_product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    db.delete(db_product)
+    db.commit()
+    
+    return {"message": f"Product {product_id} deleted successfully"}
--- routes/products.py
+++ routes/products.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from config.db import get_db
-from controllers.products import create_product 
+from controllers.products import create_product, get_all_products, update_product, delete_product
 from pydantic import BaseModel
 
 router = APIRouter()
 
 class ProductCreate(BaseModel):
-    id: str
     name: str
     material: str
     color: str
@@ -21,3 +20,31 @@ class ProductCreate(BaseModel):
 @router.post('/products')
 def create_product_api(product: ProductCreate, db: Session = Depends(get_db)):
     return create_product(db, product.dict())
+
+
+@router.get("/getAllProducts")
+def fetch_all_products(db:Session = Depends(get_db)):
+    return get_all_products(db)
+
+
+class ProductUpdate(BaseModel):
+    price: float
+    stock_qty: int
+
+@router.put('/updateProduct/{product_id}')
+def update_product_api(
+    product_id: str, 
+    update_data: ProductUpdate, 
+    db: Session = Depends(get_db)
+):
+    return update_product(
+        db, 
+        product_id, 
+        update_data.price, 
+        update_data.stock_qty
+    )
+
+
+@router.delete('/deleteProduct/{product_id}')
+def delete_product_api(product_id: str, db: Session = Depends(get_db)):
+    return delete_product(db, product_id)
